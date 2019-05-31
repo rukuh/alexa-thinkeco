934a5405f962bbbae2a7d3e171906dc865be5818
Luke Hyun <luke.hyun@gmail.com>
2019-05-30 23:19:07 -0400

    Update request to API

--- src/index.js
+++ src/index.js
@@ -1,205 +1,208 @@
-var https       = require( 'https' )
-  , AlexaSkill  = require( './AlexaSkill' )
-  , env         = require( './env.js' )
-  , APP_ID      = process.env.APP_ID
-  , username    = process.env.USERNAME
-  , password    = process.env.PASSWORD;
-
-const applianceId = { 'living room': '1966', 'bedroom': '1967' };
-
-function getCookiesFromThinkEco( location, temperature, state, callback ) {
-  var options = {
-    host   : 'mymodlet.com',
-    path   : '/Account/Login?loginForm.Email=' + username + '&loginForm.Password=' + encodeURIComponent( password ) + '&loginForm.RememberMe=True&ReturnUrl=',
-    port   : '443',
-    method : 'POST',
-    headers: {
-      'Accept'      : 'application/json, text/javascript, */*; q=0.01',
-      'Content-Type': 'application/json; charset=UTF-8'
-    }
-  };
-
-  var req = https.request( options, function( res ) {
-    res.on( 'data', function( body ) {
-      getThermostat( res.headers[ 'set-cookie' ], location, temperature, state, callback );
-    });
-  });
-
-  req.on( 'error', function( e ) {
-    console.log( 'Request error: ' + e.message );
-  });
-
-  req.end();
+var https = require('https'),
+	alexaSkill = require('./AlexaSkill'),
+	env = require('./env.js'),
+	appID = process.env.appID,
+	applianceIDs = process.env.applianceIDs,
+	username = process.env.username,
+	password = process.env.password;
+	
+function login() {
+	var body = {
+		data: JSON.stringify({
+			Email: username,
+			Password: password
+		})
+	};
+	
+	var options = {
+		host: 'web.mymodlet.com',
+		path: '/Account/Login',
+		method: 'post',
+		headers: { 'Content-Type':'application/json' }
+	};
+	
+	return new Promise((resolve, reject) => {
+		var req = https.request(options, res => {
+			if (res.statusCode < 200 || res.statusCode >= 300) reject(new Error('login::statusCode=' + res.statusCode));
+			res.on('data', () => resolve(res.headers['set-cookie']));
+		});
+		req.on('error', err => reject(err));
+		req.write(JSON.stringify(body));
+		req.end();
+	});
 };
 
-function getThermostat( cookies, location, temperature, state, callback ) {
-  var options = {
-    host   : 'mymodlet.com',
-    path   : '/SmartAC/UserSettingsTable',
-    port   : '443',
-    method : 'POST',
-    gzip   : true,
-    headers: {
-      'Accept'        : 'application/json, text/javascript, */*; q=0.01',
-      'Content-Type'  : 'application/json; charset=UTF-8',
-      'Cookie'        : cookies.join( '; ' )
-    }
-  };
-
-  var req = https.request( options, function( res ) {
-    var response = '';
-
-    res.on( 'data', function( body ) {
-      response += body;
-    });
-
-    res.on( 'end', function() {
-      var data = JSON.parse( response ).match( /((select id="[0-9]+")|(option value="[0-9]+" selected))/g ).toString().match( /[0-9]+/g );
-      setThermostat( cookies , applianceId[ location ], temperature ? temperature : thermostatObject( data )[ applianceId[ location ] ], state, callback );
-    });
-  });
-
-  req.on( 'error', function( e ) {
-    console.log( 'Request error: ' + e.message );
-  });
-
-  req.end();
+function updateData(intent, cookies) {
+	var options = {
+		host: 'web.mymodlet.com',
+		path: '/Devices/UpdateData',
+		method: 'get',
+		gzip: true,
+		headers: { 'Content-Type': 'application/json', 'Cookie': cookies.join('; ') }
+	};
+	
+	return new Promise((resolve, reject) => {
+		var req = https.request(options, res => {
+			var response = '';
+			
+			if (res.statusCode < 200 || res.statusCode >= 300) reject(new Error('updateData::statusCode=' + res.statusCode));
+			res.on('data', body => response += body);
+			res.on('end', () => {
+				const parsedJSON = JSON.parse(JSON.parse(response));
+				const modlet = parsedJSON.Devices.find(device => device.deviceId == applianceIDs[intent.slots.location.value]);
+				const smartAC = parsedJSON.SmartACs.find(smartAC => smartAC.thermostat.associatedModletId == modlet.modletId);
+				resolve(smartAC);
+			});
+		});
+		req.on('error', err => reject(err));
+		req.end();
+	});
 }
 
-function setThermostat( cookies, location, temperature, state, callback ) {
-  var data = JSON.stringify( {
-    'applianceId'      : location,
-    'targetTemperature': temperature,
-    'thermostated'     : ( state == 'on' )
-  });
+function switchDevice(intent, cookies) {
+	var body = {
+		data: JSON.stringify({
+			id: applianceIDs[intent.slots.location.value]
+		})
+	 };
 
-  var options = {
-    host   : 'mymodlet.com',
-    path   : '/SmartAC/UserSettings',
-    port   : '443',
-    method : 'POST',
-    gzip   : true,
-    headers: {
-      'Accept'         : 'application/json, text/javascript, */*; q=0.01',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Content-Length' : Buffer.byteLength( data ),
-      'Content-Type'   : 'application/json; charset=UTF-8',
-      'Cookie'         : cookies.join( '; ' )
-    }
-  };
-
-  var req = https.request( options, function( res ) {
-    var response = '';
-
-    res.on( 'data', function( body ) {
-      response += body;
-    });
-
-    res.on( 'end', function() {
-      callback( [ temperature, state ] );
-    });
-  });
-
-  req.on( 'error', function( e ) {
-    console.log( 'Request error: ' + e.message );
-  });
-
-  req.write( data );
-
-  req.end();
+	var options = {
+		host: 'web.mymodlet.com',
+		path: `/Devices/Switch${sentenceCase(intent.slots.state.value)}`,
+		method: 'post',
+		gzip: true,
+		headers: {
+			'Content-Type': 'application/json',
+			'Cookie': cookies.join('; ')
+		}
+	};
+	
+	return new Promise((resolve, reject) => {
+		var req = https.request(options, res => {
+			var response = '';
+			
+			if (res.statusCode < 200 || res.statusCode >= 300) reject(new Error('switchDevice::statusCode=' + res.statusCode));
+			res.on('data', body => response += body);
+			res.on('end', () => resolve(response));
+		});
+		req.on('error', err => reject(err));
+		req.write(JSON.stringify(body));
+		req.end();
+	});
 }
 
-function thermostatObject( data ) {
-  var object = {};
+function userSettingsUpdate(intent, cookies) {
+	var body = {
+		data: JSON.stringify({
+			'DeviceId': applianceIDs[intent.slots.location.value],
+			'TargetTemperature': intent.slots.temperature.value,
+			'IsThermostated': (intent.slots.state.value == 'on')
+		})
+	};
 
-  for( i = 0 ; i < data.length ; i++ ) {
-    var temp = data.shift();
-    object[ temp ] = data.shift();
-  }
-
-  return object;
+	var options = {
+		host   : 'web.mymodlet.com',
+		path   : '/Devices/UserSettingsUpdate',
+		method : 'post',
+		headers: { 'Content-Type': 'application/json', 'Cookie': cookies.join('; ') }
+	};
+	
+	return new Promise((resolve, reject) => {
+		var req = https.request(options, res => {
+			var response = '';
+			
+			if (res.statusCode < 200 || res.statusCode >= 300) reject(new Error('userSettingsUpdate::statusCode=' + res.statusCode));
+			res.on('data', body => response += body);
+			res.on('end', () => resolve(response));
+		});
+		req.on('error', err => reject(err));
+		req.write(JSON.stringify(body));
+		req.end();
+	});
 }
 
-function upperCase( string ) {
-  return string[ 0 ].toUpperCase() + string.slice( 1 );
+var handleThermostatRequest = async (intent, session, response) => {
+	console.log(JSON.stringify(intent));
+	const cookies = await login();
+	const data = await updateData(intent, cookies);
+	await userSettingsUpdate(intent, cookies);
+	if (data) {
+		var text = data;
+		var cardText = `The ${intent.slots.location.value} thermostat is set to: ${intent.slots.temperature.value}`;
+	} else {
+		var text = 'That value does not exist.'
+		var cardText = text;
+	}
+
+	var heading = `${sentenceCase(intent.slots.location.value)} thermostat turned ${intent.slots.state.value}`;
+	response.tellWithCard(`${sentenceCase(intent.slots.location.value)} thermostat set to ${intent.slots.temperature.value}`, heading, cardText);
 }
 
-var handleThermostatRequest = function( intent, session, response ) {
-  console.log( intent.toString() );
-  getCookiesFromThinkEco( intent.slots.location.value, intent.slots.temperature.value, 'on', function( data ) {
-    if( data ) {
-      var text = data;
-      var cardText = 'The ' + intent.slots.location.value + ' thermostat is set to: ' + text[ 0 ];
-    } else {
-      var text = 'That value does not exist.'
-      var cardText = text;
-    }
+var handleThermostateRequest = async (intent, session, response) => {
+	console.log(JSON.stringify(intent));
+	const cookies = await login();
+	const data = await updateData(intent, cookies);
+	await switchDevice(intent, cookies);
+	if (data) {
+		var text = data;
+		var cardText = `The ${intent.slots.location.value} AC turned: ${data.modlet.isOn ? 'on' : 'off'}`;
+	} else {
+		var text = 'That value does not exist.'
+		var cardText = text;
+	}
 
-    var heading = upperCase( intent.slots.location.value ) + ' thermostat turned ' + text[ 1 ];
-    response.tellWithCard( upperCase( intent.slots.location.value ) + ' thermostat set to ' + text[ 0 ], heading, cardText );
-  });
-};
+	var heading = `${sentenceCase(intent.slots.location.value)} AC ${intent.slots.state.value}`;
+	response.tellWithCard(`${sentenceCase(intent.slots.location.value)} AC turned ${data.modlet.isOn ? 'on' : 'off'}`, heading, cardText);
+}
 
-var handleThermostateRequest = function( intent, session, response ) {
-  console.log( intent.toString() );
-  getCookiesFromThinkEco( intent.slots.location.value, '', intent.slots.state.value, function( data ) {
-    if( data ) {
-      var text = data;
-      var cardText = 'The ' + intent.slots.location.value + ' AC turned: ' + text[ 1 ];
-    } else {
-      var text = 'That value does not exist.'
-      var cardText = text;
-    }
+var ThinkEco = function () {
+	alexaSkill.call(this, appID);
+}
 
-    var heading = upperCase( intent.slots.location.value ) + ' AC ' + text[ 1 ];
-    response.tellWithCard( upperCase( intent.slots.location.value ) + ' AC turned ' + text[ 1 ], heading, cardText );
-  });
-};
-
-var ThinkEco = function() {
-  AlexaSkill.call( this, APP_ID );
-};
-
-ThinkEco.prototype = Object.create( AlexaSkill.prototype );
+ThinkEco.prototype = Object.create(alexaSkill.prototype);
 ThinkEco.prototype.constructor = ThinkEco;
 
-ThinkEco.prototype.eventHandlers.onSessionStarted = function( sessionStartedRequest, session ) {
-  // What happens when the session starts? Optional
-  console.log( "onSessionStarted requestId: " + sessionStartedRequest.requestId
-      + ", sessionId: " + session.sessionId );
-};
+ThinkEco.prototype.eventHandlers.onSessionStarted = (sessionStartedRequest, session) => {
+	// What happens when the session starts? Optional
+	console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId
+			+ ", sessionId: " + session.sessionId);
+}
 
-ThinkEco.prototype.eventHandlers.onLaunch = function( launchRequest, session, response ) {
-  // This is when they launch the skill but don't specify what they want. Prompt
-  // them for their bus stop
-  var output = 'Welcome to Think Eco. ' +
-    'You can say things like set the living room thermostat to 77 or turn off the bedroom AC.';
+ThinkEco.prototype.eventHandlers.onLaunch = (launchRequest, session, response) => {
+	// This is when they launch the skill but don't specify what they want. Prompt
+	// them for their bus stop
+	var output = 'Welcome to Think Eco. ' +
+		'You can say things like set the living room thermostat to 77 or turn off the bedroom AC.';
 
-  var reprompt = 'What thermostat would you like to change?';
+	var reprompt = 'What thermostat would you like to change?';
 
-  response.ask( output, reprompt );
+	response.ask(output, reprompt);
 
-  console.log( "onLaunch requestId: " + launchRequest.requestId
-      + ", sessionId: " + session.sessionId );
-};
+	console.log("onLaunch requestId: " + launchRequest.requestId
+			+ ", sessionId: " + session.sessionId);
+}
 
 ThinkEco.prototype.intentHandlers = {
-  GetThermostatIntent: function( intent, session, response ) {
-    handleThermostatRequest( intent, session, response );
-  },
+	GetThermostatIntent: (intent, session, response) => {
+		handleThermostatRequest(intent, session, response);
+	},
 
-  GetThermostateIntent: function( intent, session, response ) {
-    handleThermostateRequest( intent, session, response );
-  },
+	GetThermostateIntent: (intent, session, response) => {
+		handleThermostateRequest(intent, session, response);
+	},
 
-  HelpIntent: function( intent, session, response ) {
-    var speechOutput = 'You can say things like set the living room thermostat to 77 or turn off the bed room AC.' +
-      'What thermostat would you like to change?';
-    response.ask( speechOutput );
-  }
-};
+	HelpIntent: (intent, session, response) => {
+		var speechOutput = 'You can say things like set the living room thermostat to 77 or turn off the bed room AC.' +
+			'What thermostat would you like to change?';
+		response.ask(speechOutput);
+	}
+}
 
-exports.handler = function( event, context ) {
-    var skill = new ThinkEco();
-    skill.execute( event, context );
-};
+function sentenceCase(string) {
+	return string[0].toUpperCase() + string.slice(1);
+}
+
+exports.handler = (event, context) => {
+		var skill = new ThinkEco();
+		skill.execute(event, context);
+}
